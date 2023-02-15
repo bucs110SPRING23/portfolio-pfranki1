4a19347876caab082c04cf48500519349cccb52b
Your Name <you@example.com>
2023-02-15 17:58:11 -0500

    2/15 exercise

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -5,10 +5,15 @@ pygame.init()
 screen=pygame.display.set_mode()
 screen.fill("white")
 pygame.display.flip()
-pygame.draw.circle(screen,"blue", [700,200], 80)
-pygame.draw.circle(screen,"blue", [700,400], 125)
-pygame.draw.circle(screen,"blue", [700,650], 170)
+
+dimensions= screen.get_size()
+print(dimensions)
+
+pygame.draw.circle(screen,"black", [700,200], 80)
+pygame.draw.circle(screen,"black", [700,400], 125)
+pygame.draw.circle(screen,"black", [700,680], 170)
+pygame.draw.circle(screen,"white", [700,200], 70)
+pygame.draw.circle(screen,"white", [700,400], 115)
+pygame.draw.circle(screen,"white", [700,680], 160)
 pygame.display.flip()
 pygame.time.wait(2000)
-
-
