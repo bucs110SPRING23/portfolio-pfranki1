6002c48d62257242fb807b4369285c8e932453c4
Your Name <you@example.com>
2023-03-08 17:59:32 -0500

    3/8 patterns exercise

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,3 +1,14 @@
-star_pyramid=int(input("Number of rows:"))
+def star_pyramid(lines):
+    for i in range(1,lines+1):
+        print("*"*i)
+    
+def rstar_pyramid(lines):
+    for i in range(lines, 0, -1):
+        print("*"*i)
 
+#(start, stop, *)
 
+height=int(input("Enter pyramid height:"))
+
+star_pyramid(height)
+rstar_pyramid(height)
