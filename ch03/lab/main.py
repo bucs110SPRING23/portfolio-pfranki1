c25e09261d6f6a66fa45697e0170dcbbd27acf13
Your Name <you@example.com>
2023-05-04 13:17:09 -0400

    labs fixed

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -2,15 +2,12 @@ import pygame
 import random
 import math
 
-pygame.init()
-
 window=pygame.display.set_mode()
-pygame.display.flip()
 size=pygame.display.get_window_size()
-print(size)
 
-window.fill("blue")
-pygame.draw.circle(window, "pink", [700,450], 450)
+window.fill("black")
+pygame.display.flip()
+pygame.draw.circle(window, "white", [size[0]/2,size[1]/2], size[1]/2)
 pygame.draw.line(window, "black",(0,size[1]/2),(size[0],size[1]/2),5)
 pygame.draw.line(window, "black",(size[0]/2,0),(size[0]/2,size[1]),5)
 pygame.display.flip()
@@ -18,14 +15,14 @@ pygame.time.wait(3000)
 pygame.display.flip()
 
 for i in range(10):
-    x1=random.randrange(0,size[0])
-    y1=random.randrange(0,size[1])
-    distance_from_center=math.hypot(x1-size[0]/2, y1-size[1]/2)
+    y=random.randrange(0,size[1])
+    x=random.randrange(0,size[0])
+    distance_from_center=math.hypot(x-size[0]/2, y-size[1]/2)
     in_circle=(distance_from_center <= size[1]/2)
     if in_circle:
-        pygame.draw.circle(window, "green", (x1,y1),15)
+        pygame.draw.circle(window, "green", (x,y),10)
     else:
-        pygame.draw.circle(window, "red", (x1,y1), 15)
+        pygame.draw.circle(window, "red", (x,y), 10)
     pygame.display.flip()
     pygame.time.wait(1000)
 pygame.time.wait(3000)
